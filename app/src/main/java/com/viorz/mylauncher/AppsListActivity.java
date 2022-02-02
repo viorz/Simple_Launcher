1d09362beee3e5ad319dfd9520386d7531245fa7
viorzez@gmail.com <45625590+viorz@users.noreply.github.com>
2022-02-02 22:03:55 +0300

    insert new file

---
+++ app/src/main/java/com/viorz/mylauncher/AppsListActivity.java
@@ -0,0 +1,11 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppsListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_list);
+    }
+}
